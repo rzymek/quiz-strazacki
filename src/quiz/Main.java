3cd5ac85aacccd55c07181cc1ac6e2bb5df09fc0
rzymek <eerzymek@gmail.com>
2013-03-06 19:40:43 +0100

    initial import

---
+++ src/quiz/Main.java
@@ -0,0 +1,226 @@
+package quiz;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+public class Main {
+	private final static List<String> ACCEPTED = Arrays.asList(ImageIO.getReaderFileSuffixes());
+	private static final Pattern ANSWER_REGEX = Pattern.compile("\\W([a-c])\\W");
+	
+	private JLabel results;
+	private JFrame frame;
+	private JLabel img;
+
+	private int index = -1;
+	private ArrayList<ImageIcon> images;
+
+	private List<File> files;
+
+	private int good=0;
+
+	private int bad=0;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					Main window = new Main();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Main() {
+		initialize();
+		try {
+			File dir = new File("img");
+			FilenameFilter filter = new FilenameFilter() {
+				@Override
+				public boolean accept(File dir, String name) {
+					int dot = name.lastIndexOf('.');
+					if (dot == -1) {
+						return false;
+					}
+					String ext = name.substring(dot + 1);
+					return ACCEPTED.contains(ext.toLowerCase());
+				}
+			};
+			files = Arrays.asList(dir.listFiles(filter));
+			if (files.isEmpty()) {
+				img.setText("<html>Brak obrazków w katalogu " + dir.getAbsolutePath() + "<br>" + "Obsługiwane pliki: "
+						+ ACCEPTED);
+			}
+			checkNames();
+			Collections.shuffle(files);
+			images = new ArrayList<ImageIcon>(files.size());
+			Dimension dim = new Dimension(0, 0);
+			for (File file : files) {
+				BufferedImage img = ImageIO.read(file);
+				if (img == null) {
+					throw new RuntimeException("Bład odczytu:" + file);
+				}
+				ImageIcon icon = new ImageIcon(img);
+				images.add(icon);
+				dim.width = Math.max(dim.width, icon.getIconWidth());
+				dim.height = Math.max(dim.height, icon.getIconHeight());
+			}
+			img.setMinimumSize(dim);
+			img.setPreferredSize(dim);
+
+			results = new JLabel("");
+			frame.getContentPane().add(results, BorderLayout.NORTH);
+			nextImg();
+			frame.pack();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			String msg = ex.getMessage();
+			if(msg == null || msg.isEmpty()) {
+				msg = ex.toString();
+			}
+			JOptionPane.showMessageDialog(frame, msg);
+			System.exit(1);
+		}
+	}
+
+	private void checkNames() {
+		for (File file : files) {
+			String name = file.getName();
+			if(!ANSWER_REGEX.matcher(name).find()){
+				throw new RuntimeException("Nazwa pliku nie zawiera odpowiedzi: "+name);
+			}
+		}
+	}
+
+	private void nextImg() {
+		index++;
+		int no = Math.min(index+1, images.size());
+		int left = Math.max(0, files.size()-index-1);
+		results.setText(String.format("<html>Dobrze: %d, Źle: %d<br>Pytanie: %d, Pozostało: %d", good, bad, no, left));
+		if (index >= images.size()) {
+			img.setText("Koniec");
+			img.setIcon(null);		
+			disable(frame);
+		} else {
+			ImageIcon icon = images.get(index);
+			img.setIcon(icon);
+		}
+	}
+
+	private void disable(Container c) {
+		c.setEnabled(false);
+		Component[] components = c.getComponents();
+		for (Component component : components) {
+			component.setEnabled(false);
+			if(component instanceof Container) {
+				disable((Container) component);
+			}
+		}
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setTitle("Quiz");
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		img = new JLabel("");
+		img.setHorizontalTextPosition(SwingConstants.CENTER);
+		img.setHorizontalAlignment(SwingConstants.CENTER);
+		frame.getContentPane().add(img, BorderLayout.CENTER);
+
+		JPanel btnPanel = new JPanel();
+		btnPanel.setPreferredSize(new Dimension(10, 100));
+		frame.getContentPane().add(btnPanel, BorderLayout.SOUTH);
+		btnPanel.setLayout(new GridLayout(0, 3, 0, 0));
+
+		JButton a = new JButton("A");
+		a.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				answer("a");
+			}
+		});
+		a.setFont(new Font("Dialog", Font.BOLD, 20));
+		btnPanel.add(a);
+
+		JButton b = new JButton("B");
+		b.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				answer("b");
+			}
+		});
+		b.setFont(new Font("Dialog", Font.BOLD, 20));
+		btnPanel.add(b);
+
+		JButton c = new JButton("C");
+		c.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				answer("c");
+			}
+		});
+		c.setFont(new Font("Dialog", Font.BOLD, 20));
+		btnPanel.add(c);
+	}
+
+
+	protected void answer(String answer) {
+		if(index >= files.size()) {
+			return;
+		}
+		String name = files.get(index).getName();
+		Matcher macher = ANSWER_REGEX.matcher(name);
+		if (macher.find()) {
+			String filenameAnswer = macher.group(1);
+			if (answer.equalsIgnoreCase(filenameAnswer)) {
+				good++;
+			} else {
+				bad++;
+			}
+		} else {
+			JOptionPane.showMessageDialog(frame, "Nazwa pliku bez odpowiedzi: " + name);
+		}
+		nextImg();
+	}
+
+
+}
