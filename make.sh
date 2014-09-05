134632ecefcbd72bbf95c6be2b97c1ee1ab6a095
rzymek <rzymek@gmail.com>
2014-09-05 22:13:59 +0200

    poprawione literówki w nazwach katalogów

--- make.sh
+++ make.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-find . -name *.png | cut -d/ -f3- | awk -f src/dir2json.awk > tests.json
+find . -name *.png | cut -d/ -f3- |sort| awk -f src/dir2json.awk > tests.json
 awk '/^@/ { file=substr($0,2); while(getline < file) { print } ;next} {print}' src/template.html > index.html
 rm tests.json
